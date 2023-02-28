376a30ef4d389a52c29670c96c534eb0e5ab3467
Bohan Zhang <zhangbohan@buaa.edu.cn>
2023-02-28 17:33:15 +0800

    [expr]: fix cross-sectional

--- experiment/simulation/cross-sectional/simulate.R
+++ experiment/simulation/cross-sectional/simulate.R
@@ -1,98 +1,193 @@
 library(dplyr)
-source('qp.R')
+library(DiscreteRecon)
+set.seed(42)
 
-# Domain Setting
-incoherent_domain = expand.grid(0:1, 0:1, 0:2)
-incoherent_string = do.call(paste0, incoherent_domain)
-coherent_domain = expand.grid(0:1, 0:1)
-coherent_domain$Var3 = coherent_domain$Var1 + coherent_domain$Var2
-coherent_string = do.call(paste0, coherent_domain)
 
-r = nrow(coherent_domain)
-q = nrow(incoherent_domain)
+simulate_series <- function(param_a,
+                            n,
+                            intercept = 0.0,
+                            innovation_mu = c(0, 0),
+                            innovation_Sigma = matrix(c(0.1, 0.05, 0.05, 0.1), 2, 2)) {
+  # simulate time series using State Space model
+  error_term <- MASS::mvrnorm(n + 1, innovation_mu, innovation_Sigma)
+  # latent state
+  for (i in 2:NROW(error_term)) {
+    error_term[i, ] = error_term[i, ] + param_a * error_term[i - 1, ]
+  }
+  # pi
+  pi = 1 / (1 + 1 / exp(error_term))
+  ifelse(pi > 0.5, 1, 0)[2:(n + 1),]
+}
 
-distance <- cal_distanceMatrix(incoherent_domain, coherent_domain)
-colnames(distance) <- incoherent_string
-rownames(distance) <- coherent_string
+tpbin <- function(k,l,p,rho, n){
+  beta <- p*(1-rho)
+  alpha <- beta+rho
+  
+  tp <- 0
+  for(j in c(max(0,k+l-n):min(k,l))){
+    tp <- tp + dbinom(j,l,alpha)*dbinom(k-j,n-l,beta)
+  }
+  tp
+}
 
-history_length = 100
-window_length = 150
+
+#Log-likelihood of binomial AR(1) model:
+llbar1 <- function(par,data, n){
+  #par is vector (p,rho)
+  T <- length(data)
+  value <- -log(dbinom(data[1], n, par[1])) #full likelihood, otherwise use 0 here
+  
+  for(t in c(2:T)) {
+    value <- value-log(tpbin(data[t], data[t-1], par[1], par[2], n))
+  }
+  value
+}
+
+binARforecast <- function(data, h, total=FALSE){
+  T <- length(data)
+  maxval = 1
+  n = 1
+  if (total){ 
+    maxval = 2 
+    n = 2
+  }
+  
+  estml <- suppressWarnings(optim(c(sum(data>0)/T, 0.65), llbar1, method="L-BFGS-B", lower=c(0.0001,0.0001), upper=c(0.9999,0.9999), control=list(ndeps=c(1e-4,1e-4)), data=data, n=maxval, hessian=TRUE))
+  pestml <- estml$par[[1]]
+  rhoestml <- estml$par[[2]]
+  
+  forecasts <- array(0, c(h, maxval+1))
+  for(i in c(1:h)){
+    for(k in c(0:maxval)){
+      forecasts[i,k+1] <- tpbin(k, data[T],pestml,rhoestml^h, maxval)
+    }
+  }
+  forecasts <- t(apply(forecasts, 1, function(x){x/sum(x)}))
+  forecasts[1,]
+}
+
+
+
+
+history_length = 150
+window_length = 300
 test_length = 30
 
 n = history_length+window_length+test_length
 
 library(foreach)
-cl <- parallel::makeCluster(4)
+cl <- parallel::makeCluster(8)
 doParallel::registerDoParallel(cl)
 
-cal_singleBase <- function(hist){
-  # generate base forecast
-  f1 <- glarma_base_forecast(hist[, 1])
-  f2 <- glarma_base_forecast(hist[, 2])
-  f3 <- glarma_base_forecast(rowSums(hist), total = TRUE)
-  list(base=apply(expand.grid(f1, f2, f3), 1, prod),
-       bu=apply(expand.grid(f1, f2), 1, prod)
-       #prob=list(f1, f2, f3)
-       )
-}
 
-cal_singleBase2 <- function(hist){
+cal_singleBase <- function(hist){
   # generate base forecast
   f1 <- binARforecast(hist[, 1], 1)
   f2 <- binARforecast(hist[, 2], 1)
   f3 <- binARforecast(rowSums(hist), 1, total = TRUE)
-  list(base=apply(expand.grid(f1, f2, f3), 1, prod),
-       bu=apply(expand.grid(f1, f2), 1, prod)
-       #prob=list(f1, f2, f3)
-  )
+  list(f1, f2, f3)
 }
 
+library(dplyr)
 
 cal_Base <- function(series, history_length, window_length){
-  library(dplyr)
-  cal_singleBase <- cal_singleBase2
-  trainBase <- foreach(i=1:(window_length), .export=c('binARforecast', 'llbar1', 'tpbin')) %dopar% 
-    cal_singleBase(series[i:(i+history_length),])
-  base <- sapply(trainBase, function(x){x$base})
-  bu <- sapply(trainBase, function(x){x$bu})
-  real <- cons_realDummy(series[(history_length+1):(window_length+history_length),], coherent_domain)
-  list(base=base, real=real, bu=bu)
+  trainBase <- list()
+  for (i in 1:window_length){
+    trainBase[[i]] <- cal_singleBase(series[i:(i+history_length),])
+  }
+  base <- list()
+  base[[1]] <- t(sapply(trainBase, function(x){x[[3]]}))
+  colnames(base[[1]]) <- c("0", "1", "2")
+  base[[2]] <- t(sapply(trainBase, function(x){x[[1]]}))
+  colnames(base[[2]]) <- c("0", "1")
+  base[[3]] <- t(sapply(trainBase, function(x){x[[2]]}))
+  colnames(base[[3]]) <- c("0", "1")
+  base
 }
+
+s_mat <- rbind(c(1, 1), diag(2))
+domain <- rbind(c(0, 0), c(1, 1))
 
 # brier_score of reconciled forecast
 
-res <- list()
-for (i in (length(res)+1):(length(res)+20)){
-  res[[i]] <- list()
+res <- foreach(i=1:1000, .packages = c("DiscreteRecon"), 
+               .export = c("simulate_series", "llbar1", "binARforecast", "tpbin")) %dopar% {
+  output <- list()
   a1 = runif(1, 0.4, 0.5)
   a2 = runif(1, 0.3, 0.5)
-  res[[i]]$a <- c(a1, a2)
-  series <- simulate_series(res[[i]]$a, n)
-  res[[i]]$train <- try(cal_Base(series, history_length, window_length), silent = TRUE)
-  res[[i]]$test <- try(cal_Base(series[(window_length+1): n,], history_length, test_length), silent = TRUE)
-  if (is.element('try-error', class(res[[i]]$train)) | is.element('try-error', class(res[[i]]$test))){
-    print(i)
-    res[[i]] <- list()
-    next
+  output$a <- c(a1, a2)
+  series <- simulate_series(output$a, n)
+  output$train <- try(cal_Base(series, history_length, window_length), silent = TRUE)
+  output$test <- try(cal_Base(series[(window_length+1): n,], history_length, test_length), silent = TRUE)
+  if (is.element('try-error', class(output$train)) | is.element('try-error', class(output$test))){
+    print(class(output$train))
+    output <- list()
+    return(output)
   }
-  metric <- tibble(bs=numeric(), method=character(), lambda=numeric())
-  res[[i]]$Ahat <- list()
-  for (lambda in c(0, 0.001, 0.005, 0.01)){
-    Amat <- 
-      opt_fun(res[[i]]$train$base, res[[i]]$train$real, distance, lambda = lambda)
-    metric <- metric %>% 
-      add_row(lambda=lambda, method='bu', bs=brier_score(res[[i]]$test$bu, res[[i]]$test$real)) %>%
-      add_row(lambda=lambda, method='reconciliation', bs=brier_score(Amat %*% res[[i]]$test$base, res[[i]]$test$real))
-    Amat[Amat<1e-10] = 0
-    res[[i]]$Ahat[[as.character(lambda)]] <- Amat
-  }
-  res[[i]]$series <- series
-  res[[i]]$metric <- metric
+  
+  train_dhts <- dhts(series[(history_length+1): (history_length+window_length),],
+                     s_mat = s_mat,
+                     domain)
+  test_dhts <- dhts(series[(history_length+window_length+1): (history_length+test_length+window_length),],
+                    s_mat = s_mat,
+                    domain)
+  dfr <- reconcile_train(output$train, train_dhts, step_wise = FALSE)
+  dfr <- reconcile(dfr, output$test, meta=train_dhts$meta)
+  
+  td <- topdown.train(train_dhts)
+  td <- reconcile(td, output$test)
+  
+  bu <- marginal2Joint(output$test, test_dhts$meta, method='bu')
+  
+  bs_vec <- function(x){ unname(c(x$series, sum(x$hierarchy))) }
+  
+  output$metric <- list(dfr=bs_vec(brier_score(dfr, test_dhts)),
+                       bu=bs_vec(brier_score(bu, test_dhts)),
+                       td=bs_vec(brier_score(td, test_dhts)),
+                       base=bs_vec(brier_score(output$test, test_dhts)))
+  output
 }
+
+# save results
+saveRDS(res, 'res.rds')
+
+# filter out errors
 res <- Filter(function(x){!is.null(x$metric)}, res)
 
-accs <- res %>% lapply(function(x){x$metric}) %>% 
-  do.call(rbind.data.frame, .)
 
-accs %>% group_by(method, lambda) %>% 
-  summarise(bs = mean(bs))
+# summarise
+accs_list <- list()
+for (m in c("base", "bu", "td", "dfr")){
+  accs_list[[m]] <- sapply(res, function(x){
+    x$metric[[m]]
+  })
+}
+accs_sum <- data.frame(lapply(accs_list, rowMeans))
+row.names(accs_sum) <- c("y_0", "y_1", "y_2", "Y")
+accs_sum
+
+library(tsutils)
+
+
+
+plot_data <- list()
+for (i in 1:4){
+  plot_data[[i]] <- sapply(accs_list, function(x){
+    x[i,]
+  })
+}
+
+pdf(file="mcb.pdf", width = 12, height = 4,
+    pointsize = 16)
+par(mfrow=c(1,3))
+nemenyi(plot_data[[1]], plottype = "vmcb", 
+        labels = c("Base", "Bottom-up", "Top-down", "DFR"), 
+        main = "MCB Test for total series")
+nemenyi(rbind(plot_data[[2]], plot_data[[3]]), plottype = "vmcb", 
+        labels = c("Base", "Bottom-up", "Top-down", "DFR"), 
+        main = "MCB Test for bottom series")
+nemenyi(plot_data[[4]], plottype = "vmcb", 
+        labels = c("Base", "Bottom-up", "Top-down", "DFR"), 
+        main = "MCB Test for hierarchy")
+dev.off()
+
